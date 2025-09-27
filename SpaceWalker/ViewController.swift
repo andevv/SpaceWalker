21c815de999e296536a691c3914cc9c92324f64e
andev <andh1006@naver.com>
2025-09-27 20:58:14 +0900

    chore: initialize Xcode project

---
+++ SpaceWalker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceWalker
+//
+//  Created by andev on 9/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
