5946ebe263c0d79674203a8560eba9ca71f743e7
andev <andh1006@naver.com>
2025-10-10 16:55:30 +0900

    feat: 앱아이콘, 탭바 및 네비게이션바 틴트컬러 적용

--- SpaceWalker/App/AppDelegate.swift
+++ SpaceWalker/App/AppDelegate.swift
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Apply accent color only to Navigation Bar and Tab Bar
+        if let accent = UIColor(named: "AccentColor_066985") {
+            // Bar button items and interactive elements on Navigation Bar
+            UINavigationBar.appearance().tintColor = accent
+            // Selected item color and interactive elements on Tab Bar
+            UITabBar.appearance().tintColor = accent
+        }
         return true
     }
 
